5a375973ca1d1c1a3bf46c2760ccff71963bacf0
dkk934 <dkking934@gmail.com>
2025-01-12 13:12:42 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: '.', // The root directory where `index.html` resides
+  build: {
+    outDir: 'dist', // The directory for production build
+    rollupOptions: {
+      input: './index.html', // Entry point
+    },
+  },
+});
